e3d5344fa9b79d2994be336f53bc48842a6613df
Andrzej Kowalski <ankowals@gmail.com>
2019-11-23 15:37:20 +0100

    test refactoring

--- src/test/java/pl/test/demo/sample/tests/SampleTest.java
+++ src/test/java/pl/test/demo/sample/tests/SampleTest.java
@@ -1,6 +1,9 @@
 package pl.test.demo.sample.tests;
 
 import com.github.database.rider.core.DBUnitRule;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
+import com.github.database.rider.core.api.dataset.SeedStrategy;
+import com.github.database.rider.core.configuration.DataSetConfig;
 import com.github.database.rider.core.util.EntityManagerProvider;
 import lombok.extern.log4j.Log4j2;
 import org.junit.Rule;
@@ -18,8 +21,13 @@ public class SampleTest extends BaseTest {
     public DBUnitRule dbUnitRule = DBUnitRule.instance(emProvider.connection());
 
     @Test
+    @ExpectedDataSet(value = "datasets/shouldContainAriaInTheDogsList/yml/expectedOwner.yml",ignoreCols = "id")
     public void shouldContainAriaInTheDogsList() {
         dbUnitRule.getDataSetExecutor().executeScript( "datasets/shouldContainAriaInTheDogsList/sql/Add_to_DOG.sql");
+
+        dbUnitRule.getDataSetExecutor()
+                .createDataSet(new DataSetConfig("datasets/shouldContainAriaInTheDogsList/csv/owner.csv")
+                        .strategy(SeedStrategy.INSERT));
 
         Dog dog = (Dog) emProvider.getEm()
                 .createQuery("select d from Dog d where d.id = 4")
